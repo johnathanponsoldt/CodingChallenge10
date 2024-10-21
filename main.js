00f3f0f47dac0ace09fad950e32d00e8b1afe94b
johnathanponsoldt <jponsoldt772@gmail.com>
2024-10-20 22:34:43 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ sizeDropdown.addEventListener('change', function (){
     const newPrice = selectedOption.getAttribute('data-price');
     priceDisplay.textContent = `Price $${newPrice}`;
 })
+
+// Task 3: Handle stock availability and disable purchase button when out of stock
+
+const purchaseButton = document.getElementById('purchase-btn');
+sizeDropdown.addEventListener('change', function () {
+    const selectedOption = sizeDropdown.options[sizeDropdown.selectedIndex];
+    const newPrice = selectedOption.getAttribute('data-price');
+    const isInstock = selectedOption.getAttribute('data-stock') === 'true';
+
+    priceDisplay.textContent = `Price: $${newPrice}`;
+    if (!isInstock) {
+        purchaseButton.disabled = true;
+ }   else {
+    purchaseButton.disabled = false;
+ }
+    
+});
